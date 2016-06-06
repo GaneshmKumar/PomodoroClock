04823cf94266edf9879cf96b1f214a32810f550f
GaneshmKumar <ganeshkumar.m1996@gmail.com>
2016-06-06 18:45:24 +0530

    responsive

--- timerJS.js
+++ timerJS.js
@@ -10,53 +10,55 @@ $(document).ready(function() {
   var clicked = 0
 var countdownTimer = ""
 var inf = ""
-$("#progress").attr("style","width:0%"); 
+$("#progress").attr("style","width:0%");
 
 $("#play").click(function() {
   $("#input").prop('disabled', true);
 
-  if(($("#input").val()))
+  time = parseInt($("#input").val())
+
+  if(time > 0 && time < 1000)
     {
-     
+
       clicked ++;
       if(clicked !== 1 && paused !== 1 && completed !== 1)
         {
-   
+
           return;
         }
       if(paused !== 1 || completed === 1)
         {
-   
+
           completed  = 0;
           percent = 0
           sec = 59
-      timer = $("#input").val();  
-     
+      timer = $("#input").val();
+
       total = timer * 59;
       min = timer - 1;
-     
+
       $("#progress").attr("aria-valuemin","0");
           $("#progress").attr("style","width:0%");
       $("#progress").attr("aria-valuemax",total);
 }
-     
-   
+
+
       countdownTimer = setInterval(secondPassed, 1000);
        function secondPassed() {
-   
+
          if(sec.toString().length < 2)
            sec = "0"+sec;
          if(min.toString().length < 2)
            min = "0"+min;
     document.getElementById('timer').innerHTML = min + " : " + sec;
-         
-         
+
+
          sec --;
          temp ++;
          percent  = (temp/total)*100;
          $("#progress").attr("style","width:"+percent+"%");
-         
-   
+
+
          if(sec == "0")
            {
              sec = 59;
@@ -71,26 +73,26 @@ $("#play").click(function() {
              completed  = 1;
              $("#progress").attr("style","width:"+percent+"%");
              clearInterval(countdownTimer);
-             
-           }
-         
-}
- 
 
-    
+           }
+
+}
+
+
+
     }
- 
+
 });// end play
-  
+
   $("#pause").click(function() {
     $("#input").prop('disabled', true);
     paused = 1
     clearInterval(countdownTimer);
     inf = setTimeout(function() {
-      
+
     }, Infinity);
   }); //end pause
-  
+
   $("#reset").click(function() {
     $("#input").val('');
     $("#input").prop('disabled', false);
@@ -104,7 +106,7 @@ $("#play").click(function() {
     $("#timer").text("00 : 00");
     $("#progress").attr("style","width:0%");
     clearInterval(countdownTimer);
-   
-    
+
+
   }); //end reset
 }); // end ready
